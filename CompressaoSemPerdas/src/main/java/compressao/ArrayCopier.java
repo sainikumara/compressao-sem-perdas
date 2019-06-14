5f9166c49f0c71cf58aa3a7feb287d357c1868ed
Kumara Saini M <saini.kumara@helsinki.fi>
2019-06-14 23:32:41 +0300

    Fix code formatting

--- CompressaoSemPerdas/src/main/java/compressao/ArrayCopier.java
+++ CompressaoSemPerdas/src/main/java/compressao/ArrayCopier.java
@@ -1,22 +1,23 @@
 package compressao;
 
 public class ArrayCopier {
-     
+
     /**
      * Create a copy of a certain range of original byte array
-     * 
+     *
      * @param originalArray
      * @param beginning
      * @param end
-     * @return new byte array that is a copy of the specified range in the original
+     * @return new byte array that is a copy of the specified range in the
+     * original
      */
     public byte[] copyOfRange(byte[] originalArray, int beginning, int end) {
         byte[] newArray = new byte[end - beginning];
-        
-        for (int i = 0; i < end-beginning; i++) {
+
+        for (int i = 0; i < end - beginning; i++) {
             newArray[i] = originalArray[beginning + i];
         }
-        
+
         return newArray;
     }
 }
